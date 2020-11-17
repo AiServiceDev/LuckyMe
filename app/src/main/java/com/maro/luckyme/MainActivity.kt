f203570c7310cca17c096143a458241121dcf90d
sunchulbaek <sunchulbaek@ncsoft.com>
2020-11-18 08:02:10 +0900

    빌드 환경 정리

--- app/src/main/java/com/maro/luckyme/MainActivity.kt
+++ app/src/main/java/com/maro/luckyme/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.maro.luckyme
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.maro.luckyme.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
